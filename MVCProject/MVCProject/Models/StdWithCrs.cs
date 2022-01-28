ad06fbdb4db502f33b456cd5d2c884b27ddc7de4
mhgbr <mgabr0004@gmail.com>
2022-01-28 02:39:10 +0200

    Done

---
+++ MVCProject/MVCProject/Models/StdWithCrs.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCProject.Models
+{
+    public class StdWithCrs
+    {
+        public int Id { get; set; }
+
+        public int Degree { get; set; }
+
+        [ForeignKey]
+        public int CrsId { get; set; }
+        [ForeignKey]
+
+        public int StId { get; set; }
+
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+
+
+    }
+}
